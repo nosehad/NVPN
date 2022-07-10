c8148de1ac0a7112d98c9f8367a1d391bcdd2c74
nosehad <77919109+nosehad@users.noreply.github.com>
2022-07-10 10:57:53 +0200

    Update logger.py
    Removed self.file.close(), because you can't log to an closed file.

--- src/com/nosehad/python/logger.py
+++ src/com/nosehad/python/logger.py
@@ -20,10 +20,5 @@ class Logger:
     def log(self, string):
         def _log():
             self.file.write(f"[{strftime('%H:%M:%S', gmtime())}] INFO: {string}\n")
-            self.file.close()
 
         threading.Thread(target=_log).start()
-
-    def save(self):
-        self.file.write(self.cur)
-        self.file.close()
